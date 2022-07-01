b4d43fdf1526b1d723bdc6e7182988a27b7ffa71
Valentyn <valentyn.postolenko98@gmail.com>
2022-07-01 08:11:33 +0300

    axios_user_post 3 - ready

--- src/services/axios.services.js
+++ src/services/axios.services.js
@@ -4,6 +4,6 @@ import axios from "axios";
 let axiosInst = axios.create({
     baseURL: "https://jsonplaceholder.typicode.com"
 })
- const getAllUsers = () => axiosInst.get('/users').then(value => value.data)
+const getAllUsers = () => axiosInst.get('/users').then(value => value.data)
 const getAllPosts = () => axiosInst.get('/posts').then(value => value.data)
 export {getAllUsers,getAllPosts}
